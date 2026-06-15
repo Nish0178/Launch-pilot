f31dbc5c92ac010308e2206868daf5ae07705cde
Nishant Trivedi <trivedinishant880@gmail.com>
2026-06-15 10:19:28 +0530

    restore clerk auth middleware

--- server/src/routes/project.routes.ts
+++ server/src/routes/project.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-// import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 import * as ProjectController from '../controllers/project.controller';
 
 const router = Router();
 
-// router.use(ClerkExpressRequireAuth());
+router.use(ClerkExpressRequireAuth());
 
 router.post('/demo', ProjectController.createDemoProject);
 router.post('/', ProjectController.createProject);
